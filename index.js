a8f94c818697c05c867d4179a2292c7e9da7c33b
clowns123 <clonws1232@gmail.comm>
2020-05-29 11:58:24 +0900

    end

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-function render() {
-  let html = '';
-  const $test = document.querySelector('.test');
-  todos.forEach(todo => {
-    $test.innerHTML += 
-    `<li id="${todo.id}">
-      <label><input type="checkbox" ${todo.completed ? 'checked' : ''}>${todo.content}</label>
-    </li>`;
-  });
+const todos = [
+  {
+    id: 3,
+    content: 'HTML',
+    completed: false
+  },
+  {
+    id: 2,
+    content: 'CSS',
+    completed: true
+  },
+  {
+    id: 1,
+    content: 'Javascript',
+    completed: false
+  }
+]
 
-  return html;
+const $ul = document.querySelector('.test')
+function render () {
+  let html = ''
+
+  todos.forEach(todo => {
+    html += `
+    <li id="${todo.id}">
+    <label><input type="checkbox" ${todo.completed ? 'checked' : ''}>${
+      todo.content
+    }</label>
+  </li>`
+  })
+
+  return html
 }
 
-console.log(render());
+console.log(render())
+
+$ul.innerHTML += render()
 /*
 <li id="3">
   <label><input type="checkbox">HTML</label>
@@ -25,5 +48,3 @@ console.log(render());
 */
 
 //아래와 같은 코드가 작성되게 view를 만들어서, index.html에 ul에 자식으로 출력하세요. 어떠한 방법을 사용해도 좋습니다.
-=======
-
